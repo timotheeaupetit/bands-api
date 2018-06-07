911677027282f13c13627a21904e1fef8e3a9822
Timothee Aupetit <timothee.aupetit@compte-nickel.fr>
2018-06-07 16:06:09 +0200

    solved problem: connection to database

--- src/main/scala/com/music/api/utils/Neo4jManager.scala
+++ src/main/scala/com/music/api/utils/Neo4jManager.scala
@@ -5,8 +5,8 @@ import org.neo4j.driver.v1._
 
 class Neo4jManager(configuration: Neo4jConfig) {
   private val uri = s"bolt://${configuration.host}:${configuration.port}"
-  private val username = configuration.user.toString
-  private val password = configuration.password.toString
+  private val username = configuration.user.getOrElse("")
+  private val password = configuration.password.getOrElse("")
 
   private lazy val driver: Driver = GraphDatabase.driver(uri, AuthTokens.basic(username, password))
 
