5b8cab74aa8a4b335ac15a579b7cf2418a2760a2
Timothee Aupetit <timothee.aupetit@gmail.com>
2018-07-01 16:06:47 +0200

    5: added unit tests for Scraper

--- src/main/scala/com/music/scraper/Scraper.scala
+++ src/main/scala/com/music/scraper/Scraper.scala
@@ -1,9 +1,15 @@
 package com.music.scraper
 
+import com.music.api.model.entities.types.{Band, Person}
 import com.music.api.utils.ProjectConfiguration._
 import net.ruippeixotog.scalascraper.browser.JsoupBrowser
+import net.ruippeixotog.scalascraper.dsl.DSL.Extract.elementList
+import net.ruippeixotog.scalascraper.dsl.DSL._
+import net.ruippeixotog.scalascraper.model.{Document, Element}
+import net.ruippeixotog.scalascraper.scraper.ContentExtractors._
 
 class Scraper(configuration: ProjectConfig) {
+
   def inferBandUrl(bandName: String): String = {
     val baseUrl = configuration.miscConfig.srcUrl
 
@@ -27,8 +33,40 @@ class Scraper(configuration: ProjectConfig) {
       }
   }
 
-  def getPageContent(url: String) = {
+  def getPage(url: String): Document = {
     val browser = JsoupBrowser()
     browser.get(url)
   }
+
+  def getArtistSection(document: Document): List[Element] = {
+    document >> elementList(".section_artist_info .artist_info")
+  }
+
+  def getDiscography(document: Document): List[Element] = {
+    document >> elementList("#disco_type_s")
+  }
+
+  def getHeaders(element: Element): List[String] = element >> elementList(".info_hdr") >> text(".info_hdr")
+
+  def getDetails(element: Element): List[Element] = element >> elementList(".info_content")
+
+  def getRawMembers(element: Element): String = element >> text(".info_content")
+
+  def process: Map[String, Element] = {
+    val url = "/Users/timotheeaupetit/Library/Preferences/IntelliJIdea2018.1/scratches/APC.html"
+    val document = getPage(url)
+
+    val info = getArtistSection(document)
+
+    val headers = getHeaders(info.head)
+
+    val details = getDetails(info.head)
+
+    headers.zip(details).toMap
+  }
+}
+
+object Scraper {
+  def getMembers(document: Document): List[Person] = ???
+  def getBand: Band = ???
 }
